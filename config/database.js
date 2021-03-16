7851a2afcf31b9beeea6502a931a3f1cce7dc5d4
mariobassem <mariobassem12@gmail.com>
2021-03-16 15:19:49 +0200

    deploy3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 module.exports = new Sequelize('postgres://eyjniqjefphkbd:bf65ee617723ae281fe3dfaf87f565f152048b4a18c42605be99271d17d713a8@ec2-54-162-119-125.compute-1.amazonaws.com:5432/daton5rsp5gqgf', {
-    host: 'localhost',
+    host: 'c2-54-162-119-125.compute-1.amazonaws.com',
     dialect: 'postgres',
     operatorsAlias: false,
     pool: {
