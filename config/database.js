4e6b4079eac43c0d66882fe3ef040fe78fe0cc4b
mariobassem <mariobassem12@gmail.com>
2021-03-16 15:27:33 +0200

    deploy3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
-module.exports = new Sequelize('postgres://eyjniqjefphkbd:bf65ee617723ae281fe3dfaf87f565f152048b4a18c42605be99271d17d713a8@ec2-54-162-119-125.compute-1.amazonaws.com:5432/daton5rsp5gqgf', {
+
+module.exports = new Sequelize({
+    username: 'eyjniqjefphkbd',
+    password: 'bf65ee617723ae281fe3dfaf87f565f152048b4a18c42605be99271d17d713a8',
     host: 'c2-54-162-119-125.compute-1.amazonaws.com',
+    port: 5432,
+    database: 'daton5rsp5gqgf',
     dialect: 'postgres',
     operatorsAlias: false,
     pool: {
@@ -10,6 +15,6 @@ module.exports = new Sequelize('postgres://eyjniqjefphkbd:bf65ee617723ae281fe3df
         idle: 10000
     },
     ssl: true
-});
+})
 
 // postgres://superluigi:mario@localhost:5432/chat
